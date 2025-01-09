55f56433d1e9768535e62aad5c29d6b894026361
Ashutosh Patil <ashutoshpa4367@gmail.com>
2025-01-09 12:35:52 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask;
-from flask import render_template;
+from flask import render_template; # This is used to render the HTML file.
+from flask_sqlalchemy import SQLAlchemy; # This is used to connect to the database.
+from datetime import datetime; # This is used to get the current date and time.
 
-app = Flask(__name__)
+app = Flask(__name__) # This is the basic way to create a Flask app.
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' # This is the database connection string.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This is to suppress a warning message.
+
+db = SQLAlchemy(app) # This is the database object.
+
+# create table to store the todo list
+class Todo(db.Model): # This is the class for the database table.
+    sno = db.Column(db.Integer, primary_key=True) # This is the primary key column.
+    title = db.Column(db.String(200), nullable=False) # This is the title column.
+    desc = db.Column(db.String(500), nullable=False) # This is the description column.
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) # This is the date column. utcnow is a function to get the current date and time.
+
+# always use this function and mention what you want to see insted of sno and title.
+    def __repr__(self): # This is the representation function.
+        return f"{self.sno} - {self.title}" # This is the representation string.
 
 @app.route('/')
 def home():
@@ -14,4 +32,6 @@ def ABCD():
 
 # This is the main function to run the app. 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000) # we can specify the port number / change port here.
+    with app.app_context(): # This is to create the database table
+        db.create_all() # This is to create the database table.
+    app.run(debug=True) # we can specify the port number / change port here.
